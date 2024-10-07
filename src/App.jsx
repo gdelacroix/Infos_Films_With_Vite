eea3546b8551d9a7eb8f3f9c7dcee1ab65088a59
Guizmohackett <46965883+gdelacroix@users.noreply.github.com>
2024-10-07 16:10:57 +0200

    config projet with vite

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+   return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="App">
+       
+        <img src="/images/dwwm_1.png" className="App-logo" alt="logo"  />
+        <p>
+        TP React Développeur Web et Web Mobile
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Rechercher des films
+        </a>
+      </div>
+  
+    </>
+  )
+}
+
+export default App
