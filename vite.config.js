06395aab198e0eba3b50d9a9572e97d212b08080
Guizmohackett <46965883+gdelacroix@users.noreply.github.com>
2024-10-17 13:49:15 +0200

    pb config pr deploiement

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/infos-film_dwwm",
+  base: "/Infos_Films_With_Vite/",
   plugins: [react()],
   build: {
     outDir: 'dist', // Définit le dossier de sortie
