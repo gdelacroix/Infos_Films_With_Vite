4566a7b42a692b2bb0d4299b4d96e7388a9613bf
Guizmohackett <46965883+gdelacroix@users.noreply.github.com>
2024-10-17 13:30:48 +0200

    nouvelle config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/infos-film_dwwm",
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Définit le dossier de sortie
+  },
 })
