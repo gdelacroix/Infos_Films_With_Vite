023817fc4f32da0becb03ccb53eb09ea7612414e
Guizmohackett <46965883+gdelacroix@users.noreply.github.com>
2024-10-17 12:52:31 +0200

    modif fichier deploiement

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Infos_Films_With_Vite/">
       <App />
     </BrowserRouter>
   </StrictMode>
